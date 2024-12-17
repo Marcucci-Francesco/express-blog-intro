cf86e89450b72ee9a76481cc1a0e5d2827d9d410
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-17 15:18:07 +0100

    feat: base project

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog')
+});
